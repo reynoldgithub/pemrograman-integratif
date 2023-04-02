3bc51faf0af3d671284f3fbcab782a50387d9a88
reynoldgithub <reynold170803@gmail.com>
2023-04-03 00:02:27 +0700

    fix: seperate input

--- grpc-node-ts-mongodb/typescript/src/client/index.ts
+++ grpc-node-ts-mongodb/typescript/src/client/index.ts
@@ -1,11 +1,11 @@
 import * as grpc from '@grpc/grpc-js'
 import * as protoLoader from '@grpc/proto-loader'
 import { ProtoGrpcType} from '../proto/user'
-import { UserServiceHandlers } from '../proto/userPackage/UserService'
 import path from 'path'
+import { UserID } from '../proto/userPackage/UserID'
 import { User } from '../proto/userPackage/User'
 import { UserWithID } from '../proto/userPackage/UserWithID'
-import { UserID } from '../proto/userPackage/UserID'
+import readline from 'readline';
 
 const PROTO_PATH : string = "../../../proto/user.proto"
 const PORT : number = 5001
@@ -20,7 +20,6 @@ const options = {
 
 const protoBuf = protoLoader.loadSync(path.resolve(__dirname, PROTO_PATH), options)
 const grpcObj = (grpc.loadPackageDefinition(protoBuf) as unknown) as ProtoGrpcType
-const userService = grpcObj.userPackage
 
 const client = new grpcObj.userPackage.UserService(
   `0.0.0.0:${PORT}`, grpc.credentials.createInsecure()
@@ -36,6 +35,22 @@ client.waitForReady(deadline, (err) => {
   onClientReady()
 })
 
+const createInput : User = {
+  name : "Putra",
+  email : "putra@gmail.com",
+  age : "24",
+  password : "password"
+}
+
+const updateInput : User = {
+  name : "Putra",
+  email : "putra@gmail.com",
+  age : "24",
+  password : "password"
+}
+
+const userId : string = "6429a01cadd3919bfcde0148" 
+
 const onClientReady = () => {
   console.log(`Client running on port ${PORT}`)
   // client.GetAll(
@@ -47,16 +62,11 @@ const onClientReady = () => {
   //     }
   //     console.log(res)
   //   }
-  // )
  
+  // )
 
   // test create user
-  // const user : User = {
-  //   name : "Anaps",
-  //   email : "anap@gmail.com",
-  //   age : "24",
-  //   password : "password"
-  // }
+  // const user : User = createInput 
   // client.createUser(user, 
   //   (err, res) => {
   //     if(err) {
@@ -71,14 +81,9 @@ const onClientReady = () => {
   // test update user
   // const user : UserWithID = {
   //   userId : {
-  //     id : "6429a01cadd3919bfcde0148"
+  //     id : userId
   //   },
-  //   user : {
-  //     name : "Anaps edited",
-  //     email : "anap@gmail.com",
-  //     age : "24",
-  //     password : "password",
-  //   }
+  //   user : updateInput 
   // }
   // client.updateUser(user, 
   //   (err, res) => {
@@ -91,16 +96,16 @@ const onClientReady = () => {
   // )
   
   // test delete user
-  const userId : UserID = {
-    id : "6429a01cadd3919bfcde0148"
-  }
-  client.deleteUser(userId, 
-    (err, res) => {
-      if(err) {
-        console.error(err)
-        return
-      }
-      console.log(res)
-    }
-  )
+  // const userId : UserID = {
+  //   id : "6429a01cadd3919bfcde0148"
+  // }
+  // client.deleteUser(userId, 
+  //   (err, res) => {
+  //     if(err) {
+  //       console.error(err)
+  //       return
+  //     }
+  //     console.log(res)
+  //   }
+  // )
 }
